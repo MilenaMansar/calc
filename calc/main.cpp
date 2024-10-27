9f7e452938c855c2b840fcaf35457c7956505a93
Milena <156203920+MilenaMansar@users.noreply.github.com>
2024-10-27 13:47:24 +0300

    main.cpp

--- calc/main.cpp
+++ calc/main.cpp
@@ -1,14 +1,61 @@
-//
-//  main.cpp
-//  calc
-//
-//  Created by Милена Мансар on 27.10.2024.
-//
-
 #include <iostream>
+#include <stack>
+#include <string>
+#include <sstream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+using namespace std;
+
+double calculate(const string& expression) {
+  stack<double> numbers;
+  stringstream ss(expression);
+  string token;
+
+  while (getline(ss, token, ' ')) {
+    if (isdigit(token[0])) { // Проверяем, является ли первый символ цифрой
+      numbers.push(stod(token)); // Преобразуем строку в число и кладём в стек
+    } else {
+      double operand2 = numbers.top(); // Взять верхнее число из стека (второе число)
+      numbers.pop(); // Удалить верхнее число
+      double operand1 = numbers.top(); // Взять число, которое было под верхним (первое число)
+      numbers.pop(); // Удалить это число
+
+      switch (token[0]) {
+        case '+':
+          numbers.push(operand1 + operand2);
+          break;
+        case '-':
+          numbers.push(operand1 - operand2);
+          break;
+        case '*':
+          numbers.push(operand1 * operand2);
+          break;
+        case '/':
+          if (operand2 == 0) {
+            throw runtime_error("Деление на ноль!");
+          }
+          numbers.push(operand1 / operand2);
+          break;
+        default:
+          throw runtime_error("Неверный оператор: " + token);
+      }
+    }
+  }
+
+  return numbers.top(); // Вернуть верхнее число (результат)
+}
+
+int main() {
+  string expression;
+  cout << "Введите выражение в польской нотации (числа и операторы разделены пробелами): ";
+  getline(cin, expression);
+
+  try {
+    double result = calculate(expression);
+    cout << "Результат: " << result << endl;
+  } catch (const runtime_error& e) {
+    cerr << "Ошибка: " << e.what() << endl;
+    return 1;
+  }
+
+  return 0;
 }
